14ecce27fa54a0f695205f306028366efa7bac68
Dizzrt <dizzrt.s@qq.com>
2025-12-22 16:31:10 +0800

    add tray icon

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,8 @@
+use tauri::{
+  menu::{Menu, MenuItem},
+  tray::TrayIconBuilder,
+};
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
@@ -9,6 +14,24 @@ pub fn run() {
             .build(),
         )?;
       }
+
+      let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+      let menu = Menu::with_items(app, &[&quit_i])?;
+
+      let _tray = TrayIconBuilder::new()
+        .icon(app.default_window_icon().unwrap().clone())
+        .menu(&menu)
+        .show_menu_on_left_click(true)
+        .on_menu_event(|app, event| match event.id.as_ref() {
+          "quit" => {
+            print!("quit menu item was clicked");
+            app.exit(0);
+          }
+          _ => {
+            print!("menu item {:?} not handled", event.id);
+          }
+        })
+        .build(app)?;
       Ok(())
     })
     .run(tauri::generate_context!())
