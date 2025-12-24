0bc527a4c5bf28743def1c073241ab0fad3e5d6f
Dizzrt <dizzrt.s@qq.com>
2025-12-24 22:05:58 +0800

    customize window closing behavior

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,40 @@
 use tauri::{
   menu::{Menu, MenuItem},
   tray::TrayIconBuilder,
+  Manager, Runtime, WindowEvent,
 };
+
+fn build_menu<R: Runtime>(app: &tauri::App<R>) -> Result<Menu<R>, tauri::Error> {
+  let menu_item_show = MenuItem::with_id(app, "show", "Show Window", true, None::<&str>)?;
+  let menu_item_quit = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+
+  let menu = Menu::with_items(app, &[&menu_item_show, &menu_item_quit])?;
+  Ok(menu)
+}
+
+fn create_tray_icon<R: Runtime>(app: &tauri::App<R>, menu: &Menu<R>) -> Result<(), tauri::Error> {
+  TrayIconBuilder::new()
+    .icon(app.default_window_icon().unwrap().clone())
+    .menu(menu)
+    .show_menu_on_left_click(true)
+    .on_menu_event(|app, event| match event.id.as_ref() {
+      "show" => {
+        print!("show menu item was clicked");
+        if let Some(window) = app.get_webview_window("main") {
+          window.show().unwrap();
+        }
+      }
+      "quit" => {
+        print!("quit menu item was clicked");
+        app.exit(0);
+      }
+      _ => {
+        print!("menu item {:?} not handled", event.id);
+      }
+    })
+    .build(app)?;
+  Ok(())
+}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -15,23 +48,30 @@ pub fn run() {
         )?;
       }
 
-      let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
-      let menu = Menu::with_items(app, &[&quit_i])?;
+      let menu = build_menu(app)?;
+      create_tray_icon(app, &menu)?;
 
-      let _tray = TrayIconBuilder::new()
-        .icon(app.default_window_icon().unwrap().clone())
-        .menu(&menu)
-        .show_menu_on_left_click(true)
-        .on_menu_event(|app, event| match event.id.as_ref() {
-          "quit" => {
-            print!("quit menu item was clicked");
-            app.exit(0);
+      // get main window and set close behavior
+      if let Some(window) = app.get_webview_window("main") {
+        // clone window object to avoid ownership problem
+        let window_clone = window.clone();
+
+        // use window event listener to handle close event
+        window.on_window_event(move |event| {
+          match event {
+            // listen close event
+            WindowEvent::CloseRequested { api, .. } => {
+              // prevent default close behavior
+              api.prevent_close();
+
+              // hide window
+              window_clone.hide().unwrap();
+            }
+            _ => {}
           }
-          _ => {
-            print!("menu item {:?} not handled", event.id);
-          }
-        })
-        .build(app)?;
+        });
+      }
+
       Ok(())
     })
     .run(tauri::generate_context!())
